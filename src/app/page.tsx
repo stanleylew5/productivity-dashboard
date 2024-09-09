fa82a5b2cb97f703cd1a244c1124dce2477de099
Stanley Lew <stanley43595@gmail.com>
2024-09-09 08:43:51 -0700

    add auth and dummy home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,8 @@
-"use client";
-
-import { useSession, signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
+import Login from "@/components/Login";
 export default function Home() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (status === "authenticated") {
-      router.push("/dashboard");
-    }
-  }, [status, router]);
-
-  if (status === "loading") {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div>
-      <h1>Welcome! Please log in.</h1>
-      {!session && (
-        <button onClick={() => signIn("google")}>Sign in with Google</button>
-      )}
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useSession, signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import loginbg from "../../public/login.jpg";
+import clock from "../../public/clock.png";
+import Image from "next/image";
+
+const Login = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/dashboard");
+    }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <Image
+        className="absolute z-[-1] h-screen w-screen"
+        src={loginbg}
+        alt="loginbg"
+      />
+      <div className="flex flex-col items-center justify-center text-white">
+        <Image
+          className="mb-[5vh] mt-[10vh] w-[8vw] invert"
+          src={clock}
+          alt="clock"
+        />
+        <p className="mb-[5vh] text-5xl text-white">Sign in to Dashboard</p>
+        <div className="rounded-md border-2 border-white bg-purple-200 px-[10vw] py-[4vh] text-purple-950">
+          {!session && (
+            <button
+              onClick={() => signIn("google")}
+              className="flex flex-row items-center gap-4 text-4xl"
+            >
+              Sign in with Google
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
