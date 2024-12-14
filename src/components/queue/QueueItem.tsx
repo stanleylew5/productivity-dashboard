19add82d5d8b29d1b408f265a17f9a10554c3779
Stanley Lew <stanley43595@gmail.com>
2024-12-13 18:38:05 -0800

    update some more

--- src/components/queue/QueueItem.tsx
+++ src/components/queue/QueueItem.tsx
@@ -1,10 +1,25 @@
-import Image from "next/image"
-const QueueItem = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import Image from "next/image";
 
-export default QueueItem
+type QueueItemProps = {
+  albumUrl: string;
+  songName: string;
+  artist: string;
+};
+
+const QueueItem: React.FC<QueueItemProps> = ({ albumUrl, songName, artist }) => {
+  return (
+    <div className="flex flex-row items-center justify-center text-center">
+      <Image
+        src={albumUrl}
+        alt="Album Cover"
+        className="mb-4 rounded-lg"
+        width={160}
+        height={160}
+      />
+      <p className="text-md font-semibold">{songName}</p>
+      <p className="text-xs text-gray-500">{artist}</p>
+    </div>
+  );
+};
+
+export default QueueItem;
