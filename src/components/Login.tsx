b2510f5da59cc6278025e90c3ae838512ba847b7
Stanley Lew <stanley43595@gmail.com>
2024-12-16 20:34:51 -0800

    fix build error

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,7 +3,6 @@
 import { useSession, signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import loginbg from "../../public/login.jpg";
 import dashboard from "../../public/dashboard.jpg";
 import clock from "../../public/clock.png";
 import Image from "next/image";
@@ -40,7 +39,7 @@ const Login = () => {
           alt="clock"
         />
         <p className="mb-[5vh] text-5xl text-white">Sign in to Dashboard</p>
-        <div className="rounded-md border-2 border-white bg-purple-200 px-[10vw] py-[4vh] text-purple-950">
+        <div className="rounded-md border-2 border-white bg-dash-orange-200 px-[10vw] py-[4vh] text-purple-950">
           {!session && (
             <button
               onClick={() => signIn("google")}
